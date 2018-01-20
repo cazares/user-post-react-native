9997c5dc1e0871578913736e96648f737a925ed6
Miguel Cazares <miguel.cazares@shipstation.com>
2018-01-19 18:24:04 -0600

    some progress

---
+++ user-post/user-post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import style from './style/user-post-style';
+
+export default class UserPost extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>TEST</Text>
+      </View>
+    );
+  }
+}
--- user-post/user-post.test.js
+++ user-post/user-post.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import App from './App';
+import UserPost from './UserPost';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
+  const rendered = renderer.create(<UserPost />).toJSON();
   expect(rendered).toBeTruthy();
 });
