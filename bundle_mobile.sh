a7ac598e5b1ae83792aa5211d4f6b8097e72d876
Miguel Cazares <miguel.cazares@shipstation.com>
2018-01-21 20:06:03 -0600

    prep for appstores

--- bundle_mobile.sh
+++ bundle_mobile.sh
@@ -1,3 +1,3 @@
 react-native bundle --platform android --dev false --entry-file $1/$1.js --bundle-output android/app/src/main/assets/$1.jsbundle --assets-dest android/app/src/main/res/
-#react-native bundle --entry-file $1/$1.js --platform ios --dev false --bundle-output ios/$1.jsbundle --assets-dest ./ios
+react-native bundle --entry-file $1/$1.js --platform ios --dev false --bundle-output ios/$1.jsbundle --assets-dest ./ios
 
