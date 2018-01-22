1021d38240ff109505bff2c4763645ab93ac0f3d
Miguel Cazares <miguel.cazares@shipstation.com>
2018-01-21 19:12:26 -0600

    render modify post screen!

--- user-post/modify-post.js
+++ user-post/modify-post.js
@@ -2,16 +2,36 @@ import React from 'react';
 import { Text, TextInput, KeyboardAvoidingView, Keyboard } from 'react-native';
 
 import style from '../style/user-post-style';
+import { isAndroid } from '../util/platform';
 
 const IPHONE_VERTICAL_OFFSET = 64;
 const ANDROID_VERTICAL_OFFSET = 80;
+const NAV_TITLE = 'Edit Post';
+const KEYBOARD_KEY = 'keyboardAvoidingViewKey';
 
 export default class ModifyPost extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: NAV_TITLE,
+    };
+  }
+
+  state = {
+    keyboardAvoidingViewKey: KEYBOARD_KEY,
+    title: null,
+    body: null,
+  }
 
   componentDidMount() {
     console.log('didmount called in post-list');
     const { title, body } = this.props.navigation.state.params.post;
     this.setState({ title, body });
+
+    this.keyboardHideListener = Keyboard.addListener(isAndroid() ? 'keyboardDidHide' : 'keyboardWillHide', this.keyboardHideListener);
+  }
+
+  componentWillUnmount() {
+    this.keyboardHideListener.remove();
   }
 
   componentDidUpdate() {
@@ -40,28 +60,30 @@ export default class ModifyPost extends React.Component {
   render() {
     const { keyboardAvoidingViewKey, title, body } = this.state;
 
-    <KeyboardAvoidingView
-      style={baseStyle.columns}
-      behavior="height"
-      keyboardVerticalOffset={this.getKeyboardVerticalOffset}
-      key={keyboardAvoidingViewKey}>
-      <Text style={style.defaultLabel}>Title:</Text>
-      <TextInput
-        onChangeText={this.onChangeTitle}
-        underlineColorAndroid='transparent'
-        blurOnSubmit={false}
-        value={title}
-        style={style.postInput}
-      />
-      <Text style={style.defaultLabel}>Body:</Text>
-      <TextInput
-        onChangeText={this.onChangeBody}
-        multiline
-        underlineColorAndroid='transparent'
-        blurOnSubmit={false}
-        value={body}
-        style={[style.postInput, style.postBodyInput]}
-      />
-    </KeyboardAvoidingView>
+    return (
+      <KeyboardAvoidingView
+        style={style.columns}
+        behavior="height"
+        keyboardVerticalOffset={this.getKeyboardVerticalOffset}
+        key={keyboardAvoidingViewKey}>
+        <Text style={style.defaultLabel}>Title:</Text>
+        <TextInput
+          onChangeText={this.onChangeTitle}
+          underlineColorAndroid='transparent'
+          blurOnSubmit={false}
+          value={title}
+          style={style.postInput}
+        />
+        <Text style={style.defaultLabel}>Body:</Text>
+        <TextInput
+          onChangeText={this.onChangeBody}
+          multiline
+          underlineColorAndroid='transparent'
+          blurOnSubmit={false}
+          value={body}
+          style={[style.postInput, style.postBodyInput]}
+        />
+      </KeyboardAvoidingView>
+    );
   }
 }
